6d5ba5a2a2ab8479196ebb7ab6a504c3411bc4e6
Valeri Esquivel <1530205@upv.edu.mx>
2020-09-05 19:32:14 -0500

    docs add

---
+++ docs/precache-manifest.b59e58c3f79a7c30c1b76a59d5e5d476.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e07922edede24f57e783027b31aa9e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a12e27b73280921184c",
+    "url": "/static/css/main.e6e99927.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a12e27b73280921184c",
+    "url": "/static/js/main.e1bac77b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
